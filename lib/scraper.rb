0f26abfbb45889d0daf5356619efa57f1e6cc24e
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-31 01:16:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
         :location => student.css("p.student-location").text , 
         :profile_url => student.css("a").attribute("href").value
       } 
-      p hash.profile_url
+      p hash[:profile_url]
      
   end
   
