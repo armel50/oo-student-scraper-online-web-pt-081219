e0c620eaf286b1924eeacad3cb048648837ab461
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-31 01:33:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
 
     @name = student_hash[:name] 
     @location = student_hash[:location]
-    
+    @profile_url = student_hash[:profile_url] if student_hash[:profile_url]
   end
 
   def self.create_from_collection(students_array)
